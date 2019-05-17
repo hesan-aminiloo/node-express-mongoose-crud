baf649038a8d79e97da07ce57cb01a67b7b3ce01
Hesan Aminiloo <hesan.aminiloo@gmail.com>
2019-05-17 15:43:37 +0430

    [FEATURE] Complete developer controller and change some configs.

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 module.exports = {
     mongo_db    :   'mongo_crud',
     mongo_ip    :   '127.0.0.1',
-    mongo_port  :   '27018'
+    mongo_port  :   '27017'
 }
--- controllers/developer.controller.js
+++ controllers/developer.controller.js
@@ -107,13 +107,63 @@ developer_controller.createDeveloper = (req, res) => {
 
 
 developer_controller.updateDeveloper = (req, res) => {
+    const { id } = req.params;
 
+    if (!id || !req.body){
+
+        res.status(400).send({
+            status: false,
+            msg: 'Please provide id and new data to update developer'
+        });
+
+    } else {
+        Developer
+            .findByIdAndUpdate(id, req.body, { upsert : true, new: true })
+
+            .then(developer => {
+                if (developer)
+                    res.json(developer);
+            })
+
+            .catch(err => {
+                console.log(err);
+                res.json({
+                    status: false,
+                    msg: 'Error updating the developer'
+                });
+            });
+    }
 };
 
 
 
 developer_controller.deleteDeveloper = (req, res) => {
+    const { id } = req.params;
 
+    if (!id){
+
+        res.status(400).send({
+            status: false,
+            msg: 'Please provide id to delete developer'
+        });
+
+    } else {
+        Developer
+            .findByIdAndDelete(id)
+
+            .then(developer => {
+                if (developer)
+                    res.json(developer);
+            })
+
+            .catch(err => {
+                console.log(err);
+                res.json({
+                    status: false,
+                    msg: 'Error deleting the developer'
+                });
+            });
+    }
 };
 
 
